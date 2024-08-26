f498b683543280ab447b63d5bd09663befa136ce
luciano Rodrigues Leandro <luciano2000rodrigues@gmail.com>
2024-08-25 22:27:00 -0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,53 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][0])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+async function btnCopiar() {
+    alert('Texto Copiado');
+    try {
+        const mensagem = document.querySelector(".mensagem");
+        await navigator.clipboard.writeText(mensagem.value);
+        alert("Texto copiado para a área de transferência!");
+    } catch (err) {
+        console.error("Falha ao copiar o texto: ", err);
+    }
+}
+
+
